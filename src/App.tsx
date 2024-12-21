faa92bb6a1182f8f7e5351ceab1d0fa989d37089
S-h-a-m-i-r <shahshamu164@gmail.com>
2024-12-22 03:32:33 +0500

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,9 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Layout from './components/layout/Layout';
-import Login from './pages/auth/Login';
-import Signup from './pages/auth/Signup';
-import ForgotPassword from './pages/auth/forgotPassword';
-import OTPIndex from './pages/auth/forgotPassword/OTPIndex';
-import Newpassword from './pages/auth/forgotPassword/Newpassword';
-import SuccessIndex from './pages/auth/forgotPassword/SuccessIndex';
+import './App.css'
+import { RouterProvider } from 'react-router-dom';
+import { router } from './routes';
 
-const App: React.FC = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route path="login" element={<Login />} />
-        <Route path="signup" element={<Signup />} />
-        <Route path="forgot-password" element={<ForgotPassword />}>
-          <Route path="otp" element={<OTPIndex />} />
-          <Route path="new-password" element={<Newpassword />} />
-          <Route path="success" element={<SuccessIndex />} />
-        </Route>
-      </Route>
-    </Routes>
-  );
-};
+function App() {
+  return <RouterProvider router={router} />;
+}
 
-export default App;
+export default App
